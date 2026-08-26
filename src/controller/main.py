24a4729ed8a111b9e0ff9e704c7bf0d4649e378d
Brayan <lsouzadiasvaz@gmail.com>
2026-08-26 19:24:24 -0300

    feat: add cors config

--- src/controller/main.py
+++ src/controller/main.py
@@ -2,17 +2,34 @@
 Inicia toda a aplicação
 """
 
-
+from src.config.settings import origin
 from fastapi import FastAPI
 from src.midlleware.base import Midlleware
 from src.handles.text import router_text
-
+from fastapi.middleware.cors import CORSMiddleware
 class AppAgent:
 
     def __init__(self)-> None:
 
         self.app = FastAPI()
         self.routes = [router_text]
+
+
+    #Cors
+    def _cors(self) -> None:
+
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=[origin],
+            allow_headers=["X-instance_user"],
+            allow_credentials=False,
+            allow_methods=["POST"]
+        )
+
+
+
+
+    
 
 
     #Adiciona o midlleware
@@ -30,6 +47,7 @@ class AppAgent:
     #Retorna o objeto do fastapi e executa os metodos
     def run(self) -> FastAPI:
 
+        self._cors()
         self._mid()
         self._router()
         return self.app
