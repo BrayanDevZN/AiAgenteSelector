448ace1f8058a84286f30612dec9fe4f9cf804ec
Brayan <lsouzadiasvaz@gmail.com>
2026-08-26 23:14:10 -0300

    fix: debug test service

--- tests/service.py
+++ tests/service.py
@@ -2,6 +2,16 @@
 Cria teste em service
 """
 if __name__ == "__main__":
-    from service.orquestration_agent import orquestration_model
+    from src.service.orquestration_agent import orquestration_model
+    from src.service.optimizate_agent import optimizate_model
+    import asyncio
 
-    print(orquestration_model(input="Cria uma calculado com loop"))
+    async def test():
+
+        print(await orquestration_model(input="Cria uma calculado com loop"))
+
+        print(await optimizate_model(input="Cria uma calculadora"))
+
+
+    asyncio.run(test())
+        
