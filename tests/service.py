080dc5bc0523ba712fd27acbacd1bae8a7cd63f4
Brayan <lsouzadiasvaz@gmail.com>
2026-08-26 23:01:18 -0300

    fix:update tests

--- tests/service.py
+++ tests/service.py
@@ -2,6 +2,6 @@
 Cria teste em service
 """
 if __name__ == "__main__":
-    from src.service.agent import orquestration_model
+    from service.orquestration_agent import orquestration_model
 
     print(orquestration_model(input="Cria uma calculado com loop"))
